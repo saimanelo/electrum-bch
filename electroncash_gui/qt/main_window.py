91176d1313bc54b4a39e797c2768837fd3e70529
Calin Culianu <calin.culianu@gmail.com>
2023-05-04 18:41:54 -0500

    Mention "Token Address" in the send tab "pay to" help.
    Since we support parsing these, mention it in the help.

--- electroncash_gui/qt/main_window.py
+++ electroncash_gui/qt/main_window.py
@@ -1641,6 +1641,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
               _("You may enter:"
                 "<ul>"
                 "<li> Bitcoin Cash <b>Address</b> <b>★</b>"
+                "<li> Bitcoin Cash <b>Token Address</b> <b>★</b>"
                 "<li> Bitcoin Legacy <b>Address</b> <b>★</b>"
                 "<li> <b>Cash Account</b> <b>★</b> e.g. <i>satoshi#123</i>"
                 "<li> <b>Contact name</b> <b>★</b> from the Contacts tab"
