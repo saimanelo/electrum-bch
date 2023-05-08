a6009703ff30d490481ab4e879ada5ec720d6626
Calin Culianu <calin.culianu@gmail.com>
2023-05-07 23:27:38 -0500

    wallet "sweep" -- try to explicitly exclude cashtokens
    This is a defensive-programming commit. We declare our protocol version
    as 1.4 to the server, so `blockchain.scripthash.listunspent` should play
    nice with us and never show us tokens. However, just to be safe, we
    explicitly ask to "exclude_tokens" and if that call fails because the
    server doesn't understand the argument, we try again without that
    parameter.
    
    This is defensive programming in case someone someday upgrades our
    Electron Cash protocol version to 1.5 (in which case tokens are included
    in the unspent list by default!)

--- electroncash/wallet.py
+++ electroncash/wallet.py
@@ -123,7 +123,14 @@ def sweep_preparations(privkeys, network, imax=100):
         else:
             address = PublicKey.from_pubkey(pubkey)
         sh = address.to_scripthash_hex()
-        u = network.synchronous_get(('blockchain.scripthash.listunspent', [sh]))
+        try:
+            # First try the protocol 1.5 version to explicitly ask for no tokens
+            u = network.synchronous_get(('blockchain.scripthash.listunspent', [sh, "exclude_tokens"]))
+        except util.ServerError as e:
+            print_error(f"sweep_preparations got exception from listunspent: \"{e}\""
+                        " (falling-back to 1.4-compatible call)")
+            # Failure; server likely doesn't know anything about tokens, fallback to 1.4 API
+            u = network.synchronous_get(('blockchain.scripthash.listunspent', [sh]))
         for item in u:
             if len(inputs) >= imax:
                 raise InputsMaxxed()
